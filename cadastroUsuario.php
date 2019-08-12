2ee6dedf620eb8a719610a579d8de98b2471ccab
luiz.ferreira <luiz.ferreira@PMO4041CGI002.pmo.local>
2019-08-12 16:02:00 -0300

    Mudanças 001

--- cadastroUsuario.php
+++ cadastroUsuario.php
@@ -12,6 +12,7 @@ and open the template in the editor.
         <link href="css/estilo.css" rel="stylesheet" type="text/css"/>
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <link href="css/materialize.min.css" rel="stylesheet" type="text/css"/>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <!--JS's-->
         <script src="js/bootstrap.min.js" type="text/javascript"></script>
         <script src="js/popper.min.js" type="text/javascript"></script>
@@ -23,78 +24,82 @@ and open the template in the editor.
         <script src="js/script.js" type="text/javascript"></script>
     </head>
     <body>
-        <nav class="cornav">
-            <div class="nav-wrapper" style="background-color: #0d47a1 !important;">
-      <a href="#!" class="">Pousada Exemplo</a>
-      <ul class="right hide-on-med-and-down">
-        <li><a href="">Logout</a></li>
-      </ul>
-    </div>
-  </nav>
-        
-           <div class="container-fluid" >
-                 <div class="sidenav-fixed " style="width:17vw">
-            <ul class="collapsible">
-    <li>
-      <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
-      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-    </li>
-    <li>
-      <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
-      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-    </li>
-    <li>
-      <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-    </li>
-  </ul>
-        
-            
-        </div>
-            <center>
-                <div class="card dlogin" style="font-family: 'Asar', regular !important; " >
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h4 class="modal-title" style="font-family: 'Asar', regular !important;">Cadastro de Usuario| Sistema de Hoteis</h4>	
-                        </div>
-                        <div class="modal-body">
-                            <form action="/examples/actions/confirmation.php" method="post">
-                                <div class="form-group">
-                                    <input type="text" class="input-field" name="user" placeholder="Usuario" required="required">		
-                                </div>
-                                <div class="form-group">
-                                    <input type="email" class="input-field" name="email" placeholder="Email" required="required">		
-                                </div>
-                                <div class="form-group">
-                                    <input type="password" class="input-field" name="senha" placeholder="Senha" required="required">	
-                                </div> 
-                                <div class="input-field form-group">
-                                    <select>
-                                        <option value="" disabled selected>Escolha o Nivel</option>
-                                        <option value="1">Administrador</option>
-                                        <option value="2">Atendente</option>
-                                    </select>
-                                    <label>Nivel</label>
-                                </div>
 
-                                <div class="form-group">
-                                    <button type="submit" class="btn tooltipped btn-outline-success " data-tooltip="Cadastrar Usuario">Cadastrar</button>
-                                    <button type="reset" class="btn  tooltipped btn-outline-danger "data-position="top" data-tooltip="Apagar Formulario">Resetar</button>
+        <div class="row Pad_top">
+            
+            <div class="container">
+                <ul id="slide-out" class="side-nav ">
+                    <li><a class="waves-effect" href="#!"><i class="material-icons">code</i>HTML5</a></li>
+                    <li><a class="waves-effect" href="#!"><i class="material-icons">flare</i>CSS3</a></li>
+                    <li><a class="waves-effect" href="#!"><i class="material-icons">desktop_mac</i>Bootstrap 3</a></li>
+                    <li><a class="waves-effect" href="#!"><i class="material-icons">laptop_mac</i>Bootstrap 4</a></li>
+                    <li><a class="waves-effect" href="#!"><i class="material-icons">dvr</i>Materialize CSS</a></li>
+                </ul>
+                <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
+                
+                <div class="container-fluid">
+                    <center>
+                        <div class="card cards" style="font-family: 'Asar', regular !important; " >
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h4 class="modal-title" style="font-family: 'Asar', regular !important;">Cadastro de Usuario| Sistema de Hoteis</h4>	
                                 </div>
-                            </form>
+                                <div class="modal-body">
+                                    <form action="/examples/actions/confirmation.php" method="post">
+                                        <div class="form-group">
+                                            <input type="text" class="input-field" name="user" placeholder="Usuario" required="required">		
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="email" class="input-field" name="email" placeholder="Email" required="required">		
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="input-field" name="senha" placeholder="Senha" required="required">	
+                                        </div> 
+                                        <div class="input-field form-group">
+                                            <select>
+                                                <option value="" disabled selected>Escolha o Nivel</option>
+                                                <option value="1">Administrador</option>
+                                                <option value="2">Atendente</option>
+                                            </select>
+                                            <label>Nivel</label>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <button type="submit" class="btn tooltipped btn-outline-success " data-tooltip="Cadastrar Usuario">Cadastrar</button>
+                                            <button type="reset" class="btn  tooltipped btn-outline-danger "data-position="top" data-tooltip="Apagar Formulario">Resetar</button>
+                                        </div>
+                                    </form>
+                                </div>
+                                <!--                        <div class="modal-footer">
+                                                            <a href="#">Esquecue a Senha?</a>
+                                                        </div>-->
+                            </div>
                         </div>
-                        <!--                        <div class="modal-footer">
-                                                    <a href="#">Esquecue a Senha?</a>
-                                                </div>-->
-                    </div>
+                    </center>
                 </div>
-            </center>
+
+            </div>
+
         </div>
-        <?php
-        // put your code here
-        ?>
-        <nav class="navbar fixed-bottom navbar-light bg-dark">
-  <a class="navbar-brand" href="#">Fixo na parte inferior</a>
-</nav>
-    </body>
+    </div>
+
+
+
+
+
+
+    <script type="text/javascript">
+        $('select').material_select();//inicial Selects
+        $('.tooltipped').tooltip();//inicia o tooltip
+        $(".button-collapse").sideNav();//inicia o sidbar
+        $(".button-collapse").sideNav();//inicia o sidbar
+    </script>
+
+    <?php
+    // put your code here
+    ?>
+    <nav class="navbar fixed-bottom navbar-light bg-dark">
+        <a class="navbar-brand" href="#">Fixo na parte inferior</a>
+    </nav>
+</body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 //inicialização dos javas 
 $(document).ready(function(){
-    $('select').formSelect();//inicial Selects
-    $('.tooltipped').tooltip();//inicia o tooltip
-    $('.collapsible').collapsible();//inicia o sidbar
+   
   });
